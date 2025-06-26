89f729c98ed9ec9d0c5b859027008d6c05b012c0
Eventa25 <starkart.visuals@gmail.com>
2025-06-26 16:44:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ const app = express();
 app.use(cors());
 app.use('/api', eventbriteRoutes);
 
+app.get('/', (req, res) => {
+  res.send('✅ Eventa backend está online e funcional.');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor ativo na porta ${PORT}`));
