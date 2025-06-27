633e80736ed50ee91647bbe8c6d908442d19c617
Eventa25 <starkart.visuals@gmail.com>
2025-06-27 20:52:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import eventbriteRoutes from './routes/eventbrite.js';
+import eventosRoutes from './routes/eventos.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
+
 app.use('/api', eventbriteRoutes);
+app.use('/api', eventosRoutes);
 
 app.get('/', (req, res) => {
   res.send('✅ Eventa backend está online e funcional.');
