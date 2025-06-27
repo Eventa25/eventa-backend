cbb3e03175a25f5a9843f36633894836c25ae000
Eventa25 <starkart.visuals@gmail.com>
2025-06-27 11:51:24 +0100

    Update eventbrite.js

--- routes/eventbrite.js
+++ routes/eventbrite.js
@@ -6,14 +6,7 @@ const EB_TOKEN = process.env.EVENTBRITE_TOKEN;
 
 router.get('/eventbrite-events', async (req, res) => {
   const { lat, lon, radius = '20km' } = req.query;
-  
   console.log('TOKEN USADO:', EB_TOKEN);
-  console.log("PARAMS ENVIADOS:", {
-    'location.latitude': lat,
-    'location.longitude': lon,
-    'location.within': radius,
-    expand: 'venue'
-  });
 
   try {
     const response = await axios.get('https://www.eventbriteapi.com/v3/events/search/', {
@@ -28,9 +21,10 @@ router.get('/eventbrite-events', async (req, res) => {
 
     res.json(response.data.events);
   } catch (error) {
-    console.error('Erro Eventbrite:', error.response?.data || error);
-    res.status(500).json({ error: 'Erro ao obter eventos da Eventbrite' });
+    console.error('Erro Eventbrite:', error?.response?.data || error.message);
+    res.status(500).json({ error: 'Erro ao obter eventos da Eventbrite', detalhe: error?.response?.data || error.message });
   }
 });
 
 export default router;
+
